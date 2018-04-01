d9d11285b42f89c4bed6eb5e86506bb83cce4844
Apoorva Joshi <xunl3kah@gmail.com>
2018-04-01 12:00:50 +0530

    Create common.h

---
+++ common.h
@@ -0,0 +1,13 @@
+#ifdef USEGLEW
+#include <GL/glew.h>
+#endif
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
