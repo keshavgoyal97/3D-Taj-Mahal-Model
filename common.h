0008b226842a7796de9cf63487a13b55a95ca092
Apoorva Joshi <xunl3kah@gmail.com>
2018-04-10 06:25:13 +0530

    Update common.h

--- common.h
+++ common.h
@@ -1,6 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <cmath>
+#include <queue>
+#include <utility>
+
+
 #ifdef USEGLEW
 #include <GL/glew.h>
 #endif
+
 #define GL_GLEXT_PROTOTYPES
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
@@ -10,4 +23,13 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
+
+#define PI 3.1415926535898
+#define Cos(th) cos(PI/180*(th))
+#define Sin(th) sin(PI/180*(th))
+#define DEF_D 5
+
+
+
 #endif
