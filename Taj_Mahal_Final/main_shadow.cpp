f74e9651a93350e01a060eb1f7c6f0e4c939c561
Apoorva Joshi <xunl3kah@gmail.com>
2018-04-16 13:51:26 +0530

    Update main_shadow.cpp

--- Taj_Mahal_Final/main_shadow.cpp
+++ Taj_Mahal_Final/main_shadow.cpp
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <GLUT/GLUT.h>
+#ifdef USEGLEW
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
 //#include "draw_compo.hpp"
 #include "Keyboard_mouse_handle.hpp"
 #include "Lighting.hpp"
